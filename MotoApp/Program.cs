770d3c9234989ae378876aeb755a035316f3e876
damianololo <dolszewski.1988@gmail.com>
2022-07-08 12:20:43 +0200

    Add project files.

---
+++ MotoApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using MotoApp.Data;
+using MotoApp.Entities;
+using MotoApp.Repositories;
+
+var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext());
+AddEmployee(employeeRepository);
+AddManagers(employeeRepository);
+WriteAllToConsole(employeeRepository);
+
+static void AddEmployee(IWriteRepository<Employee> employeeRepository)
+{
+    employeeRepository.Add(new Employee { FirstName = "Damian" });
+    employeeRepository.Add(new Employee { FirstName = "Krzysztof" });
+    employeeRepository.Add(new Employee { FirstName = "Szymon" });
+    employeeRepository.Save();
+}
+
+static void AddManagers (IWriteRepository<Manager> managerRepository)
+{
+    managerRepository.Add(new Manager { FirstName = "Adam" });
+    managerRepository.Add(new Manager { FirstName = "Zuzia" });
+    managerRepository.Save();
+}
+static void WriteAllToConsole(IReadRepository<IEntity> repository)
+{
+    var items = repository.GetAll();
+    foreach (var item in items)
+    {
+        Console.WriteLine(item);
+    }
+}
